050731ff1853d1cfb482ecddea45aeac8003eb28
[Samridhi Gupta] <samridhigupta1944@gmail.com>
2026-05-31 23:36:54 +0530

    updated

--- road.js
+++ road.js
@@ -1,56 +1,87 @@
-class Road{
-    constructor(x,width,laneCount=3){
-        this.x=x;
-        this.width=width;
-        this.laneCount=laneCount;
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-        this.left=x-width/2;
-        this.right=x+width/2;
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 
-        const infinity=1000000;
-        this.top=-infinity;
-        this.bottom=infinity;
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-        const topLeft={x:this.left,y:this.top};
-        const topRight={x:this.right,y:this.top};
-        const bottomLeft={x:this.left,y:this.bottom};
-        const bottomRight={x:this.right,y:this.bottom};
-        this.borders=[
-            [topLeft,bottomLeft],
-            [topRight,bottomRight]
-        ];
-    }
-
-    getLaneCenter(laneIndex){
-        const laneWidth=this.width/this.laneCount;
-        return this.left+laneWidth/2+
-            Math.min(laneIndex,this.laneCount-1)*laneWidth;
-    }
-
-    draw(ctx){
-        ctx.lineWidth=5;
-        ctx.strokeStyle="white";
-
-        for(let i=1;i<=this.laneCount-1;i++){
-            const x=lerp(
-                this.left,
-                this.right,
-                i/this.laneCount
-            );
-            
-            ctx.setLineDash([20,20]);
-            ctx.beginPath();
-            ctx.moveTo(x,this.top);
-            ctx.lineTo(x,this.bottom);
-            ctx.stroke();
+const N=1;
+const cars=generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.1);
         }
-
-        ctx.setLineDash([]);
-        this.borders.forEach(border=>{
-            ctx.beginPath();
-            ctx.moveTo(border[0].x,border[0].y);
-            ctx.lineTo(border[1].x,border[1].y);
-            ctx.stroke();
-        });
     }
+}
+
+const traffic=[
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
+];
+
+animate();
+
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"));
+    }
+    return cars;
+}
+
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[]);
+    }
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
+    bestCar=cars.find(
+        c=>c.y==Math.min(
+            ...cars.map(c=>c.y)
+        ));
+
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
+
+    carCtx.save();
+    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
+    }
+    carCtx.globalAlpha=0.2;
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx,"blue",true);
+
+    carCtx.restore();
+
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,bestCar.brain);
+    requestAnimationFrame(animate);
 }
--- visualizer.js
+++ visualizer.js
@@ -1,4 +1,4 @@
-﻿class Visualizer{
+﻿﻿class Visualizer{
     static drawNetwork(ctx,network){
         const margin=50;
         const left=margin;
